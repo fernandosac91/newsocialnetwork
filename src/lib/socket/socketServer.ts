b8fc6b1a85c51ee58f34d330e75cef9901cd32ec
fernandosac91 <83208606+fernandosac91@users.noreply.github.com>
2025-05-17 20:55:38 +0200

    Implement Socket.IO server initialization

--- src/lib/socket/socketServer.ts
+++ src/lib/socket/socketServer.ts
@@ -1,1 +1,16 @@
- 
+import { Server as HTTPServer } from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+
+export const initSocketServer = (server: HTTPServer) => {
+  const io = new SocketIOServer(server);
+
+  io.on('connection', (socket) => {
+    console.log('Client connected:', socket.id);
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
+    });
+  });
+
+  return io;
+};
